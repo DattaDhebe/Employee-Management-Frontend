2bcacaf0219230d7ab64f76308d2c7896a29da2a
DattaDhebe <dattadhebe75@gmail.com>
2020-07-13 23:54:38 +0000

    Refactored : Login Page to adjust Container for more Responsive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route,Redirect } from 'react-router-dom';
-import { Login } from './Components/login/login';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import SignIn from './Components/login/login';
 
 class App extends Component {
   render() {
     return (
       <div>
       <Router>
-      <Route exact path="/" component={Login} />
+      <Route exact path="/" component={SignIn} />
       </Router>
       </div>
     );
--- src/Components/login/login.jsx
+++ src/Components/login/login.jsx
@@ -1,136 +1,113 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import "./login.scss";
-import { Link } from '@material-ui/core';
-import { Grid } from "@material-ui/core";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="#">
+        Employee Management 
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-const emailRegex = RegExp(/^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,3})?$/);
-const passwordRegex = RegExp(/((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,})/);
-const formValid = formErrors => {
-  let valid = true;
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-  Object.values(formErrors).forEach(value => {
-    value.length > 0 && (valid = false);
-  });
+export default function SignIn() {
+  const classes = useStyles();
 
-  return valid;
-};
-
-export class Login extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state={
-        EmailId:null,
-        Password:null,
-
-        formErrors: {
-          EmailId: "",
-          Password: ""
-        }
-      }
-    }
-    handleChange= (e)=> { 
-      console.log(e.target.value);
-      this.setState({[e.target.name]:e.target.value});  
-      console.log(this.state);
-      e.preventDefault();
-      const { name, value } = e.target;
-      let formErrors = this.state.formErrors;
-  
-      switch (name) {
-        case "EmailId":
-          formErrors.EmailId = emailRegex.test(value) ? "" : "Invalid Email Id";
-          break;
-  
-        case "Password":
-          formErrors.Password = passwordRegex.test(value) ?  "" : "Invalid Password";
-          break;
-  
-        default:
-          break;
-      }
-  
-      this.setState({ formErrors, [name]: value }, () => console.log(this.state));
-   };
-
-    login=(e) => {
-      e.preventDefault();
-      console.log(this.state);
-      let requestData ={
-        EmailId:this.state.EmailId,
-        Password:this.state.Password
-      }
-      
-      // service.login(requestData).then((json)=>{
-      //   this.props.history.push("/dashboard");
-      //   console.log("responce data==>",json);
-      // if(json.data.status==='Success'){  
-      // alert('Login Sucessfull !!');  
-      // }   
-        
-      // }).catch((err)=>{
-      //   console.log(err);
-        
-      // })
-
-    }
-    render() 
-    {
-      const { formErrors } = this.state;
-      return (
-        <form class="logincontainer">
-            <p class="title" align="center">
-            <Typography component="h1" variant="h5">
-             Login
-            </Typography>
-            </p>
-              <div className="text">
-               <TextField 
-               className={formErrors.EmailId.length > 0 ? "error" : null}
-               name="EmailId" 
-               type="email" 
-               label="Email Id" 
-               variant="outlined" 
-               value={this.state.EmailId}
-               onChange={this.handleChange} 
-                required/>
-                <div className="error">{formErrors.EmailId.length > 0 && (<span className="errorMessage">{formErrors.EmailId}</span>)} 
-                </div> 
-                </div>
-
-                <div className="text">
-                <TextField 
-                className={formErrors.Password.length > 0 ? "error" : null}
-                name="Password" 
-                type="password" 
-                label="Password"
-                 variant="outlined" 
-                 onChange={this.handleChange} 
-                 value={this.state.Password} required/>
-                <div className="error">{formErrors.Password.length > 0 && (<span className="errorMessage" >{formErrors.Password}</span>)}
-                </div>
-                </div>
-                
-                <Grid item xs={12} className="footers">
-                <div className="btn">
-                 <Button 
-                 variant="contained" 
-                 color="primary" 
-                 onClick={this.login} 
-                 className="btn">
-                 Login
-                  </Button>
-                  </div>
-                   {/* <Alert severity="error">Please fill all the Details</Alert> */}
-                  </Grid>
-
-                  <div className="link">
-                  <Link href="/register"  variant="body2">  {"Don't have an account? Register"}</Link>
-                     </div>
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+         
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
         </form>
-      );
-    }
-  }
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
