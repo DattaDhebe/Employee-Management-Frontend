260390e878dd2014facb48da11b6b000cd302b8c
DattaDhebe <dattadhebe75@gmail.com>
2020-07-15 23:44:49 +0000

    Refactored : made responsive by Applying axios for use of RESTful API.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SignUp from './Components/SignUp/SignUp';
 import { AddEmployee } from './Components/Dashboard/AddEmployee';
 import { Home } from './Components/Dashboard/Home';
 import { Update } from './Components/Dashboard/Update'
+import axios from 'axios';
 
 
 class App extends Component {
@@ -13,10 +14,15 @@ class App extends Component {
   getUser = (e) => {
     e.preventDefault();
     const email = e.target.elements.email.value;
-    const password = e.target.elements.password.value;
-
-    console.log(email);
-    console.log(password);
+    //const password = e.target.elements.password.value;
+    //axios.get(`my Rest api link"/${email}&${password}`),
+      axios.get(`https://api.github.com/users/${email}`)
+      .then((res) => { 
+        console.log(res);
+        })
+    
+    //console.log(email);
+    //console.log(password);
 
   }
 
